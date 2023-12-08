4b9d15bae026d5860df455a6a0bf6a47d621dd92
Cidyclay <cidyclayextreme@gmail.com>
2023-12-07 22:49:21 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Mapa />
+  </div>
+</template>
+
+<script>
+import Mapa from './Mapa.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    Mapa,
+  },
+};
+</script>
+
+<style>
+/* Estilos globais podem ser adicionados aqui se necessário */
+</style>
